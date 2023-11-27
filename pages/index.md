590b9622a489f776bab4986f28be39c3239442a3
Otoniel Reyes <otoniel@otonielreyes.com>
2023-11-26 19:41:25 -0800

    Add: index page

---
+++ pages/index.md
@@ -0,0 +1,26 @@
+# Welcome to My Blog!
+
+I'm thrilled to have you here. 🌟 Feel free to explore the content and engage with the community!
+
+## Links
+
+[Home](https://otonielreyes.com/) | [Blog](https://otonielreyes.com/blog) | [Showcase](https://otonielreyes.com/showcase) | [Contact](https://otonielreyes.com/contact)
+
+## Contact Me
+
+### 📧 Email
+- [Your Email](mailto:otoniel@otonielreyes.com)
+
+### 📱 Telegram
+- [Telegram](https://t.me/gaegelys)
+
+### 📸 Instagram
+- [Instagram](https://www.instagram.com/oreyesgalay)
+
+### 💼 LinkedIn
+- [LinkedIn](https://www.linkedin.com/in/oreyesgalay)
+
+### 💻 GitHub
+- [GitHub](https://github.com/kenliten)
+
+Let's connect and keep the conversation going!
--- site/index.php
+++ site/index.php
@@ -4,6 +4,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 	<title>Otoniel Reyes' Blog</title>
 <?php
 require 'vendor/autoload.php';
@@ -47,7 +51,7 @@ if ($availableThemesContent !== false) {
 // Check if the requested route starts with '/blog/'
 if (strpos($requestedPath, '/blog/') === 0) {
     $blogEntry = str_replace('/blog/', '', $requestedPath);
-    $blogEntryPath = "https://api.github.com/repos/$githubRepo/contents/blog/$blogEntry.md";
+    $blogEntryPath = $blogEntry == '' ? "https://api.github.com/repos/$githubRepo/contents/blog/index.md" : "https://api.github.com/repos/$githubRepo/contents/blog/$blogEntry.md";
 
     // Fetch the requested blog entry content from GitHub
     $blogContent = fetchGitHubContent($blogEntryPath);
